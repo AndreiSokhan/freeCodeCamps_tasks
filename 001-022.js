eaee0b99a12b0579eafaa2cc752b83d8666e1de8
Andrei <sokhanpageproofs@gmail.com>
2022-02-12 22:23:52 +0300

    ex_1-22

---
+++ 001-022.js
@@ -0,0 +1,114 @@
+//1/
+// мой первый комментарий на freeCodeCamp
+/* 
+мой первый многострочный комментарий 
+на freeCodeCamp
+ */
+
+//4.
+var a = 9;
+
+//5.
+var myFirstName = 'Andrey';
+var myLastName = 'Sokhan';
+
+//6.
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//7.
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//8.
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//9.
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//10. 
+const sum = 10 + 10;
+
+//11.
+const difference = 45 - 33;
+
+//12.
+const product = 8 * 10;
+
+//13.
+const quotient = 66 / 33;
+
+//14.
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//15.
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//16.
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 5.7;
+
+//17.
+const product = 2.0 * 2.5;
+
+//18.
+const quotient = 4.4 / 2.0; // Change this line
+
+//19.
+const remainder = 11 % 3;
+
+//20.
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//21.
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -=15;
+c -= 1;
+
+//22.
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
