628b61fcd9404b4ffb6d67ee03193009225ee81d
Andrei <sokhanpageproofs@gmail.com>
2022-02-13 18:40:29 +0300

    ex_23-30

---
+++ 023.js
@@ -0,0 +1,36 @@
+//23.
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//24.
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//25.
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//26.
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//27.
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+//28.
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//29.
+// Only change code below this line
+const myName = "Andrey";
+const myStr = "My name is " + myName + "and I am well!";
+
+//30.
+// Change code below this line
+const someAdjective = "Smb";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
