127a35f68fbfdad1232518dc8007db32ecc63408
Andrei <sokhanpageproofs@gmail.com>
2022-02-13 21:54:31 +0300

    ex_30-36

--- 023.js
+++ 023.js
@@ -34,3 +34,44 @@ const myStr = "My name is " + myName + "and I am well!";
 const someAdjective = "Smb";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
+
+//31.
+
+
+
+//32.
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//33.
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//34.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//35.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+//36.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
