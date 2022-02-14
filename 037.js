f2f0601e0d22bd169afb3bcdc1c86dea8d19254a
Andrei <sokhanpageproofs@gmail.com>
2022-02-14 23:33:37 +0300

    Ex_43-50

--- 037.js
+++ 037.js
@@ -38,3 +38,71 @@ const myArray = [
 const myData = myArray[2][1];
 
 //43.
+// Setup
+const myArray = [
+   ["John", 23],
+   ["cat", 2]
+];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+//44.
+// Setup
+const myArray = [
+   ["John", 23],
+   ["cat", 2]
+];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
+
+//45.
+// Setup
+const myArray = [
+   ["John", 23],
+   ["dog", 3]
+];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
+
+//46.
+// Setup
+const myArray = [
+   ["John", 23],
+   ["dog", 3]
+];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+//47.
+const myList = [
+   ["Chocolate Bar", 15],
+   ["Milk", 3],
+   ["Bread", 1],
+   ["Orange", 2],
+   ["butter", 3],
+];
+
+//48.
+function reusableFunction() {
+   console.log("Hi World");
+};
+reusableFunction();
+
+// 49.
+function functionWithArgs(param1, param2) {
+   console.log(param1 + param2);
+};
+functionWithArgs(5, 3);
+functionWithArgs(8, 1);
+
+//50.
+function timesFive(time) {
+   return time * 5;
+};
+
+//51.
