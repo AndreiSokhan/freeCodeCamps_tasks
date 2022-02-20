d0c3630e596411c5e326df0f229bf77278c685df
Andrei <sokhanpageproofs@gmail.com>
2022-02-20 13:38:48 +0300

    fcc_...-80

--- 071-080.js
+++ 071-080.js
@@ -172,3 +172,30 @@ function abTest(a, b) {
 }
 
 abTest(2, 2);
+
+//79.
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+//80.
+function nextInLine(arr, item) {
+   // Only change code below this line
+   arr.push(item);
+   var removed = arr.shift();
+   return removed;
+   // Only change code above this line
+}
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
