e64661268fea37c2fdc331b5ee2d5b91d13adba3
Andrei <sokhanpageproofs@gmail.com>
2022-02-23 22:10:14 +0300

    FCC_97_added

--- 090-100.js
+++ 090-100.js
@@ -66,3 +66,23 @@ for (let i = 1; i <= 9; i += 2) {
 }
 // Only change code below this line
 
+//96.
+// Setup
+const myArray = [];
+
+for (let i = 9; i > 0; i -= 2) {
+   myArray.push(i);
+}
+// Only change code below this line
+
+//97.
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+   total += myArr[i];
+}
+// Only change code below this line
+
+//98.
